98badcb04469b48f058f29bbbf69f78b49d3ee6d
David L Whitehurst <me@dlwhitehurst.com>
2023-11-21 20:55:51 -0500

    set port to 3005, tested

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
 require("./app/routes/task.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
