5263dfbed941f076fbba4a83bec24bedc14760c1
David L Whitehurst <me@dlwhitehurst.com>
2023-11-25 20:30:06 -0500

    running on Beast, need script for local config

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.use(
     })
 );
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // database
 const db = require("./app/models");
 const Task = db.task;
